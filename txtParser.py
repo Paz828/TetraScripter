cc7c2451517896c394e596f48e87e4d892f85d9d
Dorion Boesch <dorion.boesch@gmail.com>
2024-02-09 17:03:53 -0700

    iterated string

--- txtParser.py
+++ txtParser.py
@@ -1,3 +1,10 @@
 with open('test.txt') as f:
-    contents = f.read()
-    print(contents)
+    contents = f.read().upper()
+    char_list = []
+
+    for char in contents:
+        char_list.append(char)
+
+    
+
+    print(char_list)
